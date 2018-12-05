db086e29db90436835eda5587f1515e9d25f5b33
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-12-05 11:25:20 +0300

    added functions for viewing photos for previous days

--- photos/views.py
+++ photos/views.py
@@ -20,13 +20,31 @@ def photos_of_day(request):
         </html>
         '''
     return HttpResponse(html)
+
+
 def convert_dates(dates):
-    #functions that gets the weekday number for the date.
+    # functions that gets the weekday number for the date.
 
     day_number = dt.date.weekday(dates)
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    days = ['Monday', 'Tuesday', 'Wednesday',
+        'Thursday', 'Friday', 'Saturday', 'Sunday']
 
     # returning the actual day of the week
 
     day = days[day_number]
     return day
+
+
+def past_days_photos(request, past_date):
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+        day = convert_dates(date)
+        html = f'''
+            <html>
+                <body>
+                    <h1>Photos for {day} {date.day}-{date.month}-{date.year}</h1>
+                </body>
+            </html>
+                '''
+        return HttpResponse(html)
