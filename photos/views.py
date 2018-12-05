f589a27c8a90f436226ffa04be63fe520c81a977
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-12-05 11:46:44 +0300

    created templates folder

--- photos/views.py
+++ photos/views.py
@@ -8,14 +8,6 @@ from django.http import HttpResponse
 def welcome(request):
     return HttpResponse('Welcome to my Gallary')
 
-def photos_of_day(request):
-    date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-        '''
-    return HttpResponse(html)
+def welcome(request):
+    return render(request,'index.html')
             
