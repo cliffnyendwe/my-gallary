9eaf67ab5ff9c17f6bd1e91d53fce7fb48d6f481
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-12-05 19:22:24 +0300

    added functions for saving and deleting location

--- photos/models.py
+++ photos/models.py
@@ -6,6 +6,12 @@ class location(models.Model):
     location = models.CharField(max_length=40)
 
     def __str__(self):
-        return self.location
-    class Meta:
-        ordering=['location']
+        return self.location_name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    
