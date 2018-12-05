7dab32dcb5b039d68ee86646defcc4b9fe7a724c
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-12-05 18:33:59 +0300

    created my first model class as location class

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class location(models,Model):
+    location = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.location
