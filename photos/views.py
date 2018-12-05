1b5ee9de5f515f5da57529a6f9d8d427c11c5d19
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-12-05 17:50:37 +0300

    added a navbar that has gallary link

--- photos/views.py
+++ photos/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
 import datetime as dt
+
 # Create your views here.
-from django.http import HttpResponse
-
-# create my views here
-
 def welcome(request):
-    return HttpResponse('Welcome to my Gallary')
+    return render(request, 'index.html')
 
-def welcome(request):
-    return render(request,'index.html')
-            
+def photos_of_day(request):
+    date = dt.date.today()
+    return render(request, 'all-photos/today-photos.html',{'date': date})
+
+
+
+def past_days_photos(request,past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(photos_of_day)
+
+    return render(request, 'all-photos/past-photos.html', {"date":date})
