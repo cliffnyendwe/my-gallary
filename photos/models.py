9573d55ec821fae2a560245806b6f8532644ffd2
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-12-06 23:21:56 +0300

    inserted buttons to my links

--- photos/models.py
+++ photos/models.py
@@ -1,52 +1,6 @@
-# from django.db import models
-# import datetime as dt
-
-# # Create your models here.
-# class location(models.Model):
-#     location = models.CharField(max_length =40)
-  
-#     def __str__(self):
-#         return self.location
-#     class meta:
-#         ordering =['location']
-    
-#     def save_location(self):
-#         self.save()
-
-
-# class Categorys(models.Model):
-#     Category = models.CharField(max_length =40)
-   
-
-#     def __str__(self):
-#         return self.categorys
-
-
-# class Photo(models.Model):
-#     photo = models.ImageField(upload_to = 'photos/')
-#     location =models.ForeignKey(location)
-#     category =models.ManyToManyField(Categorys, default = True)
-#     post_date =models.DateTimeField(auto_now_add=True, null=True) 
- 
-#     @classmethod
-#     def todays_photos(cls):
-#         today = dt.date.today()
-#         photos = cls.objects.filter(post_date = today)
-#         return photos
-    
-#     @classmethod
-#     def days_photos(cls,date):
-#         photos = cls.objects.filter(post_date = date)
-#         return photos
-
-#     @classmethod
-#     def search_by_title(cls,search_term):
-#         photos = cls.objects.filter(title__icontains=search_term)
-#         return photos
-
 from django.db import models
 
-# Create your models here.
+# Create my models here.
 
 class Location(models.Model):
   location = models.CharField(max_length=60)
